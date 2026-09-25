1e78c58c94cfa504e1e1a39d6c9a3aef8165300d
Nikita-Titarenko <nikitatitarenko81@gmail.com>
2026-09-25 09:33:02 +0300

    fix run_explorer.bat

--- scripts/run_explorer.bat
+++ scripts/run_explorer.bat
@@ -1,5 +1,6 @@
 @echo off
 cd /d "%~dp0"
-start "" python -m http.server 8000 --directory "%~dp0explorer"
+set "EXPLORER_DIR=%~dp0..\explorer"
+start "" python -m http.server 8000 --directory "%EXPLORER_DIR%"
 start "" http://127.0.0.1:8000/index.html
 echo Explorer started at http://127.0.0.1:8000/index.html
